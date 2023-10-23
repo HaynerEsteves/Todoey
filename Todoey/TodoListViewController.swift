5f5d1b25dce1f71df037312ca9d189af87c19198
HaynerEsteves <Hayner>
2023-10-23 16:12:02 -0300

    Create IBAction for AddItem, create alert w/ action and text field, add item to tableview array, reaload tableview

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 class TodoListViewController: UITableViewController {
 
-    let itemArray: [String] = ["buy eggs", "fazer as coisas do pai", "Estudar Swift"]
+    var itemArray: [String] = ["buy eggs", "fazer as coisas do pai", "Estudar Swift"]
     
-
-    
-    
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
+//MARK: - Add New Itens
+    
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        //to take out the value inside the closure of the alert.textfield, make a new object UTEctField() global for this IBAction.
+        var textField = UITextField()
+        
+        //create alert to later implement text filed on the alert
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        // add textfield to the alert
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create New Item"
+            //using the textfield object created for this action to use it outside the closure
+            textField = alertTextField
+        }
+        
+        //create alert button for action inside the alert "add item". is the button name
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(textField.text ?? "N fez o append")
+            self.tableView.reloadData()
+        }
+        //adding the button to the alert
+        alert.addAction(action)
+        
+        
+        
+        //presenting the textfield to the user
+        present(alert, animated: true)
+        //self.tableView.reloadData()
+    }
+    
 }
+
 
 
 //MARK: - DataSource Delegate
@@ -48,6 +81,7 @@ extension TodoListViewController {
         //print(itemArray[indexPath.row])//printing the item on the array by getting the row of the selected cell
         tableView.deselectRow(at: indexPath, animated: true)//making it stop beeing grey after selection. better behavior
         
+        //check if the
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
