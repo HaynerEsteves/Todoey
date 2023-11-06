30e52bb205518546e6490ec89f29e2b4ecbc0311
HaynerEsteves <Hayner>
2023-11-06 19:08:23 -0300

    Add Core Data, Configure Core data, Save/create, read, update and delete data on CodeData

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+
 class TodoListViewController: UITableViewController {
     
     var itemArray: [Item] = []
@@ -17,7 +19,16 @@ class TodoListViewController: UITableViewController {
     // trying to save data that is a custom data type (Item) user defafaults wont work. Trying with the FileManager.
     //1- creating filepath for saving information on the users phone
     //this is also the path for the plist(named item.plist). It will be used when necessary, but does not create the file itself. only the path
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    
+    //this is how we tap into the persistentContainer inside AppDelegate to use its context
+    //Normaly ut would be like AppDelegateObject.PersistentContainer.viewConttext, but UIApplication.shared.delegate as! AppDelegate is how we create this object. With a singleton
+    //UIApplication is the class, shared is the singleton object, and delegate is the object's delegate.
+    //then we cann use the property persist.Cont. to get the viewContext > This will be used to create an item with context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +38,13 @@ class TodoListViewController: UITableViewController {
         //}
         
         loadItems()
-   
+        
     }
     
     
     
     
-//MARK: - Add New Itens
+    //MARK: - Add New Itens
     
     @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
         //to take out the value inside the closure of the alert.textfield, make a new object UTEctField() global for this IBAction.
@@ -51,9 +62,16 @@ class TodoListViewController: UITableViewController {
         
         //create alert button for action inside the alert "add item". is the button name. to be later added to the alert
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            let newItem = Item(Description: textField.text ?? "Item não adicionado")
+            
+            //creating item using plist storage method. method File manager
+            //let newItem = Item(Description: textField.text ?? "Error adding item")
+            
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text
+            newItem.checkStatus = false
             self.itemArray.append(newItem)
-
+            
             // use of defaults 2-save object, 3-retrieve object
             //self.defaults.set(self.itemArray, forKey: K.defaultsKey)
             
@@ -73,35 +91,37 @@ class TodoListViewController: UITableViewController {
     //function to encode and create/write the plist file
     func saveItems() {
         
-        // To save itens with custom classes (Item), user defaluts wont work. Use File Manager
+        // 2- To save itens with custom classes (Item), user defaluts wont work. Use File Manager
         // after creating the URL where tha plist file will be saved. we need to encode the data, then write it (create the file)
         // Creating data Encoder
-        let encoder = PropertyListEncoder()
+        //let encoder = PropertyListEncoder()
         
         do {
             //encoding item array data so it can be written. Encode needs a Try, that needs a do-catch block
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            //let data = try encoder.encode(itemArray)
+            //try data.write(to: dataFilePath!)
+            
+            //saving data on the context to the database
+            try context.save()
+            
         } catch {
-            print("Error encoding data: \(error)")
+            print("Error saving data: \(error)")
         }
     }
+    
     
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            
-            let decoder = PropertyListDecoder()
-            
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print(error)
-            }
-            
+        //1- fetch the data inside the coreData
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+            itemArray = try context.fetch(request)//method to load the data from the Coredata.
+        }catch {
+            print("error on loading data \(error)")
         }
+        
     }
-    
 }
+
 
 
 
@@ -142,6 +162,10 @@ extension TodoListViewController {
         //since the line below is a bool, we can change the if item == false { iten = true } else {iten = false}.
         //this line changes tha state of the checkStatus upon selectioin of cell on the tableview.
         itemArray[indexPath.row].checkStatus = !itemArray[indexPath.row].checkStatus
+        
+        //to delete from the core data just specify the array position to delete it
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
         
         saveItems()
         
